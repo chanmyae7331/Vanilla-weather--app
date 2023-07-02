5b5960fd70c556bf3a2d5bab4d5073f8d1bb31b4
chanmyae7331 <123553053+chanmyae7331@users.noreply.github.com>
2023-07-02 21:34:53 +0630

    Added the temperature info

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 function displayTemperature(response) {
-let temperature = document.querySelector("#temperature");
-temperature.innerhtml = response.data.main.temp;
+    
+let temperatureElement= document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+
+temperatureElement.innerHTML =Math.round (response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML= response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 
 
 let apiKey = "037821f5e82fed94604bbc6d80f916c2";
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=London&appid={apiKey}`;
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=Yangon&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
